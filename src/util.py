223d98383e461138218ed77ac40efac422c94ff1
Poot <131973759+prootiex3@users.noreply.github.com>
2023-05-13 03:01:22 +0000

    Fixed any brokenness from the rebase

--- src/util.py
+++ src/util.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:util.py
 import os, pathlib
 
 # TODO: maybe have some kind of config?
@@ -11,17 +10,3 @@ DEFAULTS_PATH = pathlib.Path("./defaults.json")
 def error(message: str):
     print(f"ERROR: {message}")
     os._exit(1)
-=======
-import os, pathlib
-
-# TODO: maybe have some kind of config?
-#      or use defaults.json to let users
-#      choose out directory?
-OUT_DIRECTORY = pathlib.Path("./out")
-DEFAULTS_PATH = pathlib.Path("./defaults.json")
-
-
-def error(message: str):
-    print(f"ERROR: {message}")
-    os._exit(1)
->>>>>>> 29570ba (Re-organize files):src/util.py
